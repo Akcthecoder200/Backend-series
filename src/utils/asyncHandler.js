0e67d48f5003db52fbacafe1679e06914e6a5d90
Arvind Kumar <akchoudhary2411@gmail.com>
2025-01-17 01:43:14 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,11 @@
-import { Promise } from "mongoose"
-//2nd method use any method try and catch or promise..here we defining a rapper for function in utils for whenever we want ...
-const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).
-        catch((err)=>next(err)) 
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
+
+
 export {asyncHandler}
 
 
@@ -15,17 +15,3 @@ export {asyncHandler}
 
 
 
-
-
-// 1st approach
-
-// const asyncHandler=(fn)=>async (req,res,next)=>{
-//    try {
-//     await fn(req,res,next)
-//    } catch (error) {
-//     res.status(err.code || 500).json({
-//         success:false,
-//         message:err.message
-//     })
-//    }
-// }
