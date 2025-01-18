8c8e3403d016848bcd7023c5e486ab84a78e985d
Arvind Kumar <akchoudhary2411@gmail.com>
2025-01-19 01:36:06 +0530

    add refreshaccesstoken endpoint..

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,13 +9,15 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
         
-        // console.log(token);
+        //console.log(token);
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
-    
+    //    console.log(token);
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
+        // console.log(process.env.ACCESS_TOKEN_SECRET);
+        
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
         if (!user) {
