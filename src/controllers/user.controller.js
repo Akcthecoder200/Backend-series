0cfcfa347ee48913ccf1f2edfc437adb5654832b
Arvind Kumar <akchoudhary2411@gmail.com>
2025-01-23 19:15:50 +0530

    Add user accound management endpoints..update files

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -246,8 +246,133 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 
 })
+
+
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    const {oldPassword, newPassword} = req.body
+
+    
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "User fetched successfully"
+    ))
+})
+
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {fullName, email} = req.body
+
+    if (!fullName || !email) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email: email
+            }
+        },
+        {new: true}
+        
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+});
+
+
+const updateUserAvatar= asyncHandler( async (req,res)=>{
+   //via multer we can accesss file (avtar and coverimage that we have saved in local storage..)
+   
+    const avatarLocalPath= req.file?.path
+    if(!avatarLocalPath){
+        throw new ApiError(404,"Avatar file is missing")
+    }
+
+    //upload on cloudinary ..we can directly update database but get url of avatar from cloudinary ..
+    const avatar= await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400,"Error while uploading on avatar")
+    }
+   
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }
+        },
+        {new:true}//this line just change what we want in set ..no effect on other things present in database..
+    ).select("-password")//select password for not sending back..
+
+    return res
+     .status(200)
+     .json(new ApiResponse(200,user,"Cover Image updated successfully.."))
+})
+
+
+const updateUserCoverImage= asyncHandler( async (req,res)=>{
+    //via multer we can accesss file (avtar and coverimage that we have saved in local storage..)
+    
+     const coverImageLocalPath= req.file?.path
+     if(!coverImageLocalPath){
+         throw new ApiError(404,"coverImage file is missing")
+     }
+ 
+     //upload on cloudinary ..we can directly update database but get url of avatar from cloudinary ..
+     const coverImage= await uploadOnCloudinary(coverImageLocalPath)
+ 
+     if(!coverImage.url){
+         throw new ApiError(400,"Error while uploading on coverImage")
+     }
+    
+     const user=await User.findByIdAndUpdate(
+         req.user?._id,
+         {
+             $set:{
+                coverImage:coverImage.url
+             }
+         },
+         {new:true}//this line just change what we want in set ..no effect on other things present in database..
+     ).select("-password")//select password for not sending back..
+
+     return res
+     .status(200)
+     .json(new ApiResponse(200,user,"Cover Image updated successfully.."))
+ })
+
+
+
 export {registerUser
       ,loginUser,
       logoutUser,
-      refreshAccessToken
+      refreshAccessToken,
+      changeCurrentPassword,
+      getCurrentUser,
+      updateAccountDetails,updateUserAvatar
+      ,updateUserCoverImage
 }
